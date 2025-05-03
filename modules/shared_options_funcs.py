acddd48ade2dc8b9d4d191ee405b8609971daccf
AUTOMATIC <->
2025-05-03 11:40:03 +0300

    hide "00002-of-00004" models

--- modules/shared_options_funcs.py
+++ modules/shared_options_funcs.py
@@ -1,6 +1,15 @@
 import os
+import re
 
 from modules import shared
+
+
+def is_multipart_extra(x):
+    m = re.search(r"(\d+)-of-(\d+)", x)
+    if not m:
+        return False
+
+    return int(m.group(1)) > 1
 
 
 def list_llamacpp_models():
@@ -8,7 +17,7 @@ def list_llamacpp_models():
         return []
 
     models = [f for f in os.listdir(shared.opts.llamacpp_model_dir) if f.endswith(".gguf")]
-    models = [x for x in models if not ]
+    models = [x for x in models if not is_multipart_extra(x)]
     models = sorted(models)
 
     return models
