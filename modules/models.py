77b87317c4c608cc67d6b9448f64bab2379e6ac6
AUTOMATIC <->
2025-08-04 06:39:01 +0300

    exist checks for backends

--- modules/models.py
+++ modules/models.py
@@ -1,6 +1,7 @@
 import dataclasses
 import os
 import re
+import shutil
 
 from modules import shared, backend_llamacpp, backend_tabbyapi
 
@@ -35,9 +36,12 @@ def list_models():
 
     models_list = []
 
+    have_tabbyapi = shared.opts.tabbyapi_path and os.path.exists(shared.opts.tabbyapi_path)
+    have_llamacpp = shared.opts.llamacpp_exe and shutil.which(shared.opts.llamacpp_exe)
+
     for root, dirs, files in os.walk(shared.opts.model_dir):
         for filename in files:
-            if filename.endswith(".gguf"):
+            if filename.endswith(".gguf") and have_llamacpp:
                 if is_multipart_extra(filename):
                     continue
 
@@ -45,14 +49,17 @@ def list_models():
                 relative_path = os.path.relpath(full_path, shared.opts.model_dir)
                 models_list.append(ModelInfo(relative_path, shared.opts.model_dir, backend_llamacpp.BackendLlamacpp))
 
-        for dir in dirs:
-            config_fn = os.path.join(root, dir, "config.json")
-            tokenizer_config_fn = os.path.join(root, dir, "tokenizer_config.json")
+        for dirname in dirs:
+            if not have_tabbyapi:
+                break
+
+            config_fn = os.path.join(root, dirname, "config.json")
+            tokenizer_config_fn = os.path.join(root, dirname, "tokenizer_config.json")
 
             if not os.path.exists(config_fn) or not os.path.exists(tokenizer_config_fn):
                 continue
 
-            full_path = os.path.join(root, dir)
+            full_path = os.path.join(root, dirname)
             relative_path = os.path.relpath(full_path, shared.opts.model_dir)
             models_list.append(ModelInfo(relative_path, shared.opts.model_dir, backend_tabbyapi.BackendTabbyapi))
 
